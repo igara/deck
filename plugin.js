0b56c5180e28f334ffd468bf223146d7007b7d50
Syo Igarashi <igara1119@gmail.com>
2023-01-26 01:58:05 +0900

    sample

---
+++ plugin.js
@@ -0,0 +1,35 @@
+options.plugins.push(
+    RevealChalkboard,
+    RevealMenu,
+	RevealCustomControls,
+	RevealFullscreen,
+	RevealMarkdown,
+);
+
+Reveal.initialize({	
+	chalkboard: { // font-awesome.min.css must be available
+		//src: "chalkboard/chalkboard.json",
+		storage: "chalkboard-demo",
+	},
+
+	customcontrols: {
+		controls: [
+			{
+				id: 'toggle-overview',
+				title: 'Toggle overview (O)',
+				icon: '<i class="fa fa-th"></i>',
+				action: 'Reveal.toggleOverview();'
+			},
+			{
+				icon: '<i class="fa fa-pen-square"></i>',
+				title: 'Toggle chalkboard (B)',
+				action: 'RevealChalkboard.toggleChalkboard();'
+			},
+			{
+				icon: '<i class="fa fa-pen"></i>',
+				title: 'Toggle notes canvas (C)',
+				action: 'RevealChalkboard.toggleNotesCanvas();'
+			}
+		]
+	},
+});
